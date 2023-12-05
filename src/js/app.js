15fd8afba7a188075eafa1704d0b1c3e089372da
agt1984 <69942981+agt1984@users.noreply.github.com>
2023-12-05 13:37:56 +0100

    New adjustments to code

--- src/js/app.js
+++ src/js/app.js
@@ -1,31 +1,39 @@
 
-/*ARRAY BASICO DE DONDE MANIPULAREMOS SUS ELEMENTOS*/
-/*Varibles Globales */
-let miArray = ["JANDER", "ALICIA", "NEYMI", "MARCELA", "ALVARO", "JOACO", "ALBERTO", "ROXANA", "ELENA", "ALFREDO", "VICTOR", "ALBA", "HANNAH"];
+ /*ARRAY BASICO DE DONDE MANIPULAREMOS SUS ELEMENTOS*/
+    /*Varibles Globales */
+    let miArray = ["JANDER", "ALICIA", "NEYMI", "MARCELA", "ALVARO", "JOACO", "ALBERTO", "ROXANA", "ELENA", "ALFREDO", "VICTOR", "ALBA", "HANNAH"];
 
-var elementosSelecanterior = []; /* Esta variable global me va a servir para ir llevando la cuenta del indice seleccionado, es decir este llevara el registro de los coders ya seleccioandos para que no se repitan */
+    var elementosSelecanterior = []; /* Esta variable global me va a servir para ir llevando la cuenta del indice seleccionado, es decir este llevara el registro de los coders ya seleccioandos para que no se repitan */
 
 
-/*funcion para agregar un nuevo elemento al array inicial */
-/******************************************************** */
-function agregarElemento() {
-        
-        /*Obtengo el valor del imput y limpio sus espacios con el trim()*/
-        let nuevoElemento = document.getElementById("nuevoElemento").value.trim();
-  
-        /*punto de control para ver si el imput no esta vacio*/
-        if (nuevoElemento !== "") {
-          miArray.push(nuevoElemento);                         /*agrego el nuevo elmento al array con el metodo push*/
-          document.getElementById("nuevoElemento").value = ""; /*Limpia el input, estableciendo su valor en una cadena vacía. */
-          alert("CODER AGREGADO");
-        } else {
-          alert("Debes agregar un nombre sin espacios");
-        }
-        
-}
 
-// Función para mostrar los elementos del array
-function mostrarArray() {
+    /*funcion para agregar un nuevo elemento al array inicial */
+    /******************************************************** */
+    /******************************************************** */
+    function agregarElemento() {
+            
+            /*Obtengo el valor del imput y limpio sus espacios con el trim()*/
+            let nuevoElemento = document.getElementById("nuevoElemento").value.trim();
+    
+            /*punto de control para ver si el imput no esta vacio*/
+            if (nuevoElemento !== "") {
+            miArray.push(nuevoElemento);                         /*agrego el nuevo elmento al array con el metodo push*/
+            document.getElementById("nuevoElemento").value = ""; /*Limpia el input, estableciendo su valor en una cadena vacía. */
+            alert("CODER AGREGADO");
+            } else {
+            alert("Debes agregar un nombre sin espacios");
+            }
+            
+    }
+    /***********************************************************/
+    /******************************************************** */
+
+
+
+    // Función para mostrar los elementos del array
+    /******************************************************** */
+    /******************************************************** */
+    function mostrarArray() {
 
         let contenedor = document.getElementById("contenedorArray");
 
@@ -37,17 +45,23 @@ function mostrarArray() {
             let nuevoElemento = document.createElement("div");
             nuevoElemento.className = "elemento";
             nuevoElemento.textContent = elemento;
+            // Puedo asignar eventos a los elementos creados por Dom
+            nuevoElemento.onclick = function() {
+                EliminarElementosdelArrayPrincipal(this);
+            };
             contenedor.appendChild(nuevoElemento);
         });
+        console.log(miArray); //observador
+    }
+    /******************************************************** */
+    /******************************************************** */
 
-}
 
 
-  /**FUNCION SENCILLA PARA SELECCIONAR EL ELEMENTO DEL ARRAY */
+    /**FUNCION SENCILLA PARA SELECCIONAR EL ELEMENTO DEL ARRAY */
     /******************************************************** */
     /******************************************************** */
     function seleccionarElemento() {
-
 
 
         // Obtener un índice aleatorio del array
@@ -57,10 +71,15 @@ function mostrarArray() {
         //Vamos por este bloque
         let indiceAleatorio  = Math.floor(Math.random() * miArray.length); //LISTO los escojo
 
-
-
         elementosSelecanterior.push(indiceAleatorio);                      //guado el indice en global              
         let elementoSeleccionado = miArray[indiceAleatorio];               //al guardo indice seleccionado
+
+        if ( miArray.length === 1) { alert("Cuidado, solo queda un Coder");}
+        if ( miArray.length === 0) {   
+            alert("Todos han sido Selecionados");
+            console.log("todos seleccionados");
+            return;
+        }
 
         alert( elementoSeleccionado + " Ha sido Seleccionado");
 
@@ -71,25 +90,51 @@ function mostrarArray() {
         let contenedordeCajitaAleatoria2 = document.getElementById("contenedorArrayAleatorio2"); /*guardo en una variable la referencia del objeto */
         contenedordeCajitaAleatoria2.appendChild(crearCajita2); /* */
 
+        /*DEebo usar este addevent listener justo despues d crear el elemento para que no cree conflicto */
+        crearCajita2.addEventListener("click", function() {
+
+            // Elimino ls cajita directamente
+            crearCajita2.remove();
+
+        }); 
 
         
-
-
-
         let elementoEliminado = miArray.splice(indiceAleatorio, 1);       //elimino el indice seleccionado del array principal
+
         console.log(miArray);                //cnsola para monitorear
         console.log(elementosSelecanterior); //cnsola para monitorear
 
         mostrarArray();                                                   //llamo a la funcion para que mustre el aray principal
 
-
-        
     }
     /******************************************************** */
     /******************************************************** */
 
 
 
+    /**FUNCION ELIMINAR EL ELEMENTOS DEL ARRAY PRINCIPAL*/
+    /******************************************************** */
+    /******************************************************** */
+    function EliminarElementosdelArrayPrincipal(elemento) {
 
-// Mostrar los elementos iniciales
-mostrarArray();
+        // Obtener el contenido del elemento que se hizo clic
+        let contenidoElemento = elemento.textContent;
+        let indiceElemento = miArray.indexOf(contenidoElemento);
+
+         // Verificar si el elemento está en el array
+         if (indiceElemento !== -1) {
+            // Eliminar el elemento del array
+            miArray.splice(indiceElemento, 1);
+
+            // Eliminar el elemento del DOM
+            elemento.remove();
+
+            console.log("Elemento eliminado:", contenidoElemento);
+            console.log("miArray actualizado:", miArray);
+        }
+    }
+    /******************************************************** */
+    /******************************************************** */
+
+    // Mostrar los elementos iniciales
+    mostrarArray();
