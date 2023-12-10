bcd3012b29e296eb9dcec258012d4d9f0a9b8b63
Jander Gomez <jandergb.30@gmail.com>
2023-12-10 20:48:32 +0100

    modified event click refresh

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,27 @@
 import LoadUser from "./load.js";
-import iniciarTemporizador from "./main.js";
+import iniciarTemporizador, { reloadUsers } from "./main.js";
+import { users } from "./users.js";
+
 
 const btn_init = document.querySelector('.btn_init');
+const btn_refresh = document.querySelector('.btn_refresh');
+const modal = document.querySelector('.modal');
+const modal_close = document.querySelector('.modal__close');
+
+window.onload = () => LoadUser(users)
 
 
+btn_init.addEventListener("click", (e) =>{
+  e.preventDefault()
+  iniciarTemporizador();
+});
 
+btn_refresh.addEventListener("click", (e)=> {
+  e.preventDefault()
+  reloadUsers()
+});
 
-window.onload = () =>{
-  LoadUser()
-}
-
-btn_init.addEventListener("click", iniciarTemporizador);
+modal_close.addEventListener("click", (e) =>{
+  e.preventDefault()
+  modal.classList.remove('modal--show')
+});
