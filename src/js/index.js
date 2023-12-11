3eb3287ca4d7063855a81877b186a6734a3ae152
Jander Gomez <jandergb.30@gmail.com>
2023-12-11 22:52:14 +0100

    Add sounds to game in page main.html and index.js

--- src/js/index.js
+++ src/js/index.js
@@ -11,18 +11,36 @@ const btn_add = document.querySelector('.btn_add');
 
 window.onload = () => LoadUser(users)
 
+let playto = document.getElementById('playto');
+function playStart() {
+  playto.play();
+}
+
 btn_init.addEventListener("click", (e) => {
   e.preventDefault()
+  playStart()
   iniciarTemporizador();
 });
 
+let addgamer = document.getElementById('addgamer');
+function Add_Sound_Gamer() {
+  addgamer.play();
+}
+
 btn_add.addEventListener("click", (e) => {
   e.preventDefault()
+  Add_Sound_Gamer()
   addUser()
 });
 
+let restartgames = document.getElementById('restartgame');
+function restart_sound_Game(){
+  restartgames.play();
+}
+
 btn_refresh.addEventListener("click", (e) => {
   e.preventDefault()
+  restart_sound_Game()
   reloadUsers()
 });
 
@@ -34,3 +52,13 @@ modal_close.addEventListener("click", (e) => {
 // Añade un controlador de eventos click al contenedor
 let contenedor = document.getElementById("container");
 contenedor.addEventListener("click", deletedUser);
+
+let winner_sound = document.getElementById('winner_sound');
+function winner_sound_Game(){
+  winner_sound.play();
+}
+
+let modal_card = document.getElementById('modal__card');
+modal_card.addEventListener('mouseover', function(){
+  winner_sound_Game()
+})
